812ad040e12f30faa962f24515cc31f3a5972fc7
esraa <esraa.mohamed87@gmail.com>
2019-05-12 01:10:26 -0700

    Adding download step

--- src/test/java/Ramadan/Series/PageObject.java
+++ src/test/java/Ramadan/Series/PageObject.java
@@ -1,14 +1,11 @@
 package Ramadan.Series;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.Sleeper;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PageObject extends BasePage{
 	
@@ -28,20 +25,16 @@ public class PageObject extends BasePage{
 		super(driver);
 	}
 	
-	public void getDownloadLink() {
+	public String getDownloadLink() {
 		driver.get(downloadBtns.get(0).getAttribute("href"));
-		System.out.println(driver.getCurrentUrl());
 		for (int i = 0; i < links.size(); i++) {
 			if(links.get(i).getAttribute("href") != null && links.get(i).getAttribute("href").contains("https://akoam.net/download/")) {
-				System.out.println(links.get(i).getAttribute("href"));
 				driver.get(links.get(i).getAttribute("href"));
 				break;
 			}
 		}
 		WebElement timer = driver.findElement(By.id("timerHolder"));
 		waitForBrowserToLoadCompletely();
-
-		System.out.println(timer.findElement(By.tagName("a")).getAttribute("href"));
-		driver.get(timer.findElement(By.tagName("a")).getText());
+		return timer.findElement(By.tagName("a")).getAttribute("href");
 	}
 }
--- src/test/java/Ramadan/Series/Script.java
+++ src/test/java/Ramadan/Series/Script.java
@@ -1,27 +1,39 @@
 package Ramadan.Series;
 
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import Ramadan.Series.Services.BashRunner;
 
 
 public class Script extends Driver {
 	PageObject pageObj;
-	public static String[] BaseURl = { "https://rmdan.net/157114/مسلسل-الزوجة-الـ18",
-			"https://rmdan.net/157103/مسلسل-لمس-أكتاف", "https://rmdan.net/157104/مسلسل-هوجان",
-			"https://rmdan.net/157110/مسلسل-ﻵخر-نفس", "https://rmdan.net/157138/مسلسل-طلقة-حظ",
-			"https://rmdan.net/157135/مسلسل-أبو-جبل" };
+
+	@DataProvider(name = "urls")
+	public static Object[][] urls() { 
+		return new Object[][] {
+			{"https://rmdan.net/157114/مسلسل-الزوجة-الـ18" , "/Users/esraa/Desktop/test"}
+//			,
+//			{"https://rmdan.net/157103/مسلسل-لمس-أكتاف", ""},
+//			{"https://rmdan.net/157104/مسلسل-هوجان", ""},
+//			{"https://rmdan.net/157110/مسلسل-ﻵخر-نفس", ""},
+//			{"https://rmdan.net/157138/مسلسل-طلقة-حظ", ""},
+//			{"https://rmdan.net/157135/مسلسل-أبو-جبل", ""}
+		};
+	}
+
 	@BeforeTest
 	public void init() {
 		pageObj = new PageObject(driver);
 	}
-	
-	@Test
-	public void download() {
-		for (String url : BaseURl) {
-			navigate(url);	
-		}
-		
-		pageObj.getDownloadLink();
+
+	@Test(dataProvider = "urls")
+	public void download(String url, String path) {
+		navigate(url);	
+		String link = pageObj.getDownloadLink();
+		BashRunner bashRunner = new BashRunner();
+		bashRunner.runCommand(link, path);
 	}
 
 }
