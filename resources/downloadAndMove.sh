61453eea7dea400f5c41892afe8fb0593f03047c
Ahmed Youssef <ahyoussef@pm.me>
2020-04-24 01:17:57 -0700

    Downloader now changes the episode name to include the season

--- resources/downloadAndMove.sh
+++ resources/downloadAndMove.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-WGET="/usr/local/bin/wget"
-echo $1
-FILE="${1//.[eE]/.s01.e}"
-cd $2; $WGET -t 2 -a log.txt $1 -O $2/$FILE
+CURL="/usr/bin/curl"
+
+NAME=${1//.s01/}
+PATH_ARR=(${NAME//\// })
+FILE="$2${PATH_ARR[${#PATH_ARR[@]}-1]//.[eE]/.S01.E}"
+echo "DOWNLOADING: $1"
+echo "FILE NAME: $FILE"
+#cd $2; $WGET -t 2 -a log.txt $1 -O $2/$FILE
+
+$CURL -o $FILE -C - $1
 
 exit 0
